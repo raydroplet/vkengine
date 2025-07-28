9d873567a8c8b2f1e6fa4d9635302ac78e5e7962
Ray Droplet <262660609+raydroplet@users.noreply.github.com>
2025-07-27 22:12:41 -0300

    build system bug fix

--- xmake.lua
+++ xmake.lua
@@ -6,11 +6,10 @@ set_languages "c++20"
 set_defaultmode "debug"
 set_policy("build.warning", true)
 
-add_rules("mode.release", "mode.debug")
 add_requires("vulkansdk", "glfw", "glm", "tinyobjloader")
-
 add_requires("glslang", { configs = { binaryonly = true } })
 
+add_rules("mode.release", "mode.debug")
 if is_mode "debug" then
   -- local sanitize = { "-fsanitize=undefined", "-fsanitize=leak" }
   -- local warnings = { "-Wall", "-Wextra", "-Weffc++", "-Wshadow" }
@@ -35,9 +34,23 @@ target "program"
       os.setenv("ROOT_PATH", project_root)
   end)
 
+-- Custom shader compilation rule
+rule("shader_compile")
+    set_extensions(".vert", ".frag", ".comp", ".geom", ".tesc", ".tese")
+    on_buildcmd_file(function (target, batchcmds, sourcefile, opt)
+        local outputdir = "build/shaders/"
+        local outputfile = path.join(outputdir, path.filename(sourcefile) .. ".spv")
+        
+        batchcmds:mkdir(outputdir)
+        batchcmds:vrunv("glslc", {"--target-env=vulkan1.0", "-o", outputfile, sourcefile})
+        batchcmds:add_depfiles(sourcefile)
+        batchcmds:set_depmtime(os.mtime(outputfile))
+        batchcmds:set_depcache(target:targetfile() .. ".shaders")
+    end)
+
 target "shaders"
-  set_kind "shared"
-  add_rules("utils.glsl2spv", { outputdir = "build/shaders/" })
+  set_kind "phony"
+  add_rules("shader_compile")
   add_files("shaders/*.vert", "shaders/*.frag")
   add_packages "glslang"
 
